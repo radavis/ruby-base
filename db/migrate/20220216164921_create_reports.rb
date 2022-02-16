839770da7472279841a3dc4689f62b35cc639cc5
Richard Davis <rdavis.bacs@gmail.com>
2022-02-16 12:17:41 -0500

    generate reports scaffold

---
+++ db/migrate/20220216164921_create_reports.rb
@@ -0,0 +1,10 @@
+class CreateReports < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reports do |t|
+      t.text :content
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
